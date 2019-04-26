70f3c7fabd90ba90fa5fe757d4b0805962fdf799
tschmi5 <1timetyler@gmail.com>
2019-04-26 15:29:28 -0400

    Added Hours To REst

--- app/types/Restaurant.ts
+++ app/types/Restaurant.ts
@@ -2,6 +2,8 @@ import {Item} from './Item';
 
 export interface Restaurant {
     name: String,
+    happyHourStart: Number,
+    happyHourEnd: Number,
     price: String,
     rating: Number,
     address: String,
