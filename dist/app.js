5964ef7a5ce00f06d1f7a6a51b95f21914226953
tschmi5 <1timetyler@gmail.com>
2019-05-20 13:32:09 -0400

    Blah

--- dist/app.js
+++ dist/app.js
@@ -30,7 +30,7 @@ function startServer(itemsData) {
      */
     app.get('/api/items', (request, response) => __awaiter(this, void 0, void 0, function* () {
         const items = yield itemsData.getAllItems();
-        response.json({ items: items }).setHeader("Access-Control-Allow-Origin", "*");
+        response.sendStatus(418).json({ items: items }).setHeader("Access-Control-Allow-Origin", "*");
     }));
     /**
      * path: /api/items/:id
